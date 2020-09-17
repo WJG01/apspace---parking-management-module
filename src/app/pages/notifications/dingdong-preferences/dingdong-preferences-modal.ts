3acde5c617359c95831d18854ee81e5b80c54981
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-09-17 09:32:49 +0800

    fix(dingdong-preferences): removed unused import

--- src/app/pages/notifications/dingdong-preferences/dingdong-preferences-modal.ts
+++ src/app/pages/notifications/dingdong-preferences/dingdong-preferences-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalController, NavParams, ToastController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
