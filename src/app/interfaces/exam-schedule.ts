15639bc7d277b85878b62174edb1419ec6ef4a9d
jiraichi <shansp@protonmail.ch>
2021-03-29 13:13:53 +0800

    feat(exam-schedule): show the exam duration for student view

--- src/app/interfaces/exam-schedule.ts
+++ src/app/interfaces/exam-schedule.ts
@@ -6,7 +6,9 @@ export interface ExamSchedule {
   since: string;
   until: string;
   docketsDue: string;
+  endDate: string;
   appraisalsDue: string | null;
   resultDate: string | null;
   assessmentType?: string | null;
+  duration?: string | null;
 }
