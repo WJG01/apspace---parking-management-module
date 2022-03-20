b54f2c3fd9c36be7ecb5074e54246d79737e841c
farisfaisalthena <muhammad.farisfaisal03@gmail.com>
2022-03-20 17:14:58 +0800

    feat: added tabs page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard, DeauthGuard } from './guards';
+
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'tabs',
     pathMatch: 'full'
   },
   {
     path: 'login',
+    canActivate: [DeauthGuard],
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 
---
+++ src/app/pages/tabs/tab-item.ts
@@ -0,0 +1,6 @@
+export interface TabItem {
+  name: string;
+  outlineIcon: string;
+  fillIcon: string;
+  path: string;
+}
