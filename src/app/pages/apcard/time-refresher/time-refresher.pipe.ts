8ecd51059a77e698ba5fe67d28cdfcb36382a10c
farisfaisalthena <muhammad.farisfaisal03@gmail.com>
2022-03-29 11:31:38 +0800

    feat(time-refresher): use date with timezone pipe

--- src/app/pages/apcard/time-refresher/time-refresher.pipe.ts
+++ src/app/pages/apcard/time-refresher/time-refresher.pipe.ts
@@ -1,11 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { DateWithTimezonePipe } from '../../../shared/date-with-timezone/date-with-timezone.pipe';
+
 @Pipe({
   name: 'timeRefresher'
 })
-export class TimeRefresherPipe implements PipeTransform {
+export class TimeRefresherPipe extends DateWithTimezonePipe implements PipeTransform {
   // TODO: Add DateTime Pipe Here
   transform(time: string, _: any): string {
-    return time;
+    return super.transform(time, 'time');
   }
 }
