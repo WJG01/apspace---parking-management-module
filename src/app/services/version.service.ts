e7c0555124361c1d0523e0c14ee66ea57a3ad666
jiraichi <shanvaliyev@hotmail.com>
2022-05-17 08:58:35 +0800

    chore: bump version to 3.1.3

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -10,7 +10,7 @@ export class VersionService {
     private plt: Platform,
   ) { }
 
-  readonly version = '3.1.2';
+  readonly version = '3.1.3';
 
   /** Application version name. */
   get name(): string {
