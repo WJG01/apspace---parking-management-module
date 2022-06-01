ed17b5e867918870798d23fd9da1a198ba707911
Tahmid Rahman <tahmidrahman.656@gmail.com>
2022-06-01 15:40:31 +0800

    feat(more-page): added list view to more page

--- src/app/pages/more/more.page.ts
+++ src/app/pages/more/more.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from "./menu.interface";
 import { Storage } from '@ionic/storage';
-import { Network } from '@capacitor/network';
 import { Browser } from '@capacitor/browser';
 import { menu } from "./menu";
 import { Observable } from "rxjs";
 import { Role } from "../../interfaces";
 import Fuse from "fuse.js";
-import { CasTicketService } from "../../services";
-import { AlertController, NavController, Platform, ToastController } from "@ionic/angular";
+import { CasTicketService, SettingsService } from "../../services";
+import { AlertButton, AlertController, NavController, Platform } from "@ionic/angular";
+import { ComponentService } from "../../services";
+import {map} from "rxjs/operators";
 
 interface KeyIconMap { [key: string]: string; }
 
@@ -17,6 +18,7 @@ interface KeyIconMap { [key: string]: string; }
   templateUrl: './more.page.html',
   styleUrls: ['./more.page.scss'],
 })
+
 export class MorePage implements OnInit {
   keyList = [];
   keyIcon: KeyIconMap = {
@@ -29,17 +31,18 @@ export class MorePage implements OnInit {
     ['Career Centre & Corporate Training']: 'assets/img/more-icons/career-training.png',
     ['Others']: 'assets/img/more-icons/others.png'
   };
+
   menuItems: MenuItem[] = menu;
   filteredMenu = [] as MenuItem[];
 
   view$: Observable<'list' | 'cards'>;
+  fav$: Observable<MenuItem[]>; // favourite items
 
   options: Fuse.IFuseOptions<MenuItem> = {
     keys: ['title', 'tags']
   };
 
-  isMobile = this.platform.is('cordova');
-  isExpand = false;
+  isMobile = this.platform.is('capacitor');
   term: '';
   editMode = false;
 
@@ -49,7 +52,8 @@ export class MorePage implements OnInit {
     private storage: Storage,
     public navCtrl: NavController,
     private platform: Platform,
-    private toastCtrl: ToastController
+    private settings: SettingsService,
+    public component: ComponentService
   ) { }
 
   ngOnInit() {
@@ -59,8 +63,8 @@ export class MorePage implements OnInit {
         console.warn(`duplicate '${menu.id}' in menuFull`);
       }
     });
-    // this.view$ = this.settings.get$('menuUI');
-
+    this.view$ = this.settings.get$('menuUI');
+    this.view$.subscribe(res => console.log(res));
     Promise.all([
       this.storage.get('role'),
       this.storage.get('canAccessResults')
@@ -79,50 +83,23 @@ export class MorePage implements OnInit {
           }
         );
       }
-    })
 
-  }
-
-  showMore(groupKey) {
-    if (this.keyList.includes(groupKey)) {
-      const index = this.keyList.indexOf(groupKey, 0);
-      if (index > -1) {
-        this.keyList.splice(index, 1);
-      }
-
-      if (this.keyList.length === 0) {
-        this.isExpand = false;
-      }
-      return;
-    }
-
-    this.keyList.push(groupKey);
-
-    const keyIconLength = Object.keys(this.keyIcon).length;
-    if (keyIconLength === this.keyList.length) {
-      this.isExpand = true;
-    }
+      this.fav$ = this.settings.get$('favoriteItems').pipe(
+        map(favs => favs.map(fav => this.menuItems.find(menu => menu.id === fav && menu.role & role))
+          .filter(menu => menu !== undefined)),
+      );
+    });
   }
 
   logout() {
-    this.alertCtrl.create({
-      header: 'Are you sure you want to log out?',
-      cssClass: 'danger-alert',
-      buttons: [
-        {
-          text: 'Cancel',
-          cssClass: 'cancel',
-          role: 'cancel'
-        },
-        {
-          text: 'Log Out',
-          cssClass: 'main',
-          handler: () => {
-            this.navCtrl.navigateForward('/logout');
-          }
-        }
-      ]
-    }).then(alert => alert.present());
+    const btn: AlertButton = {
+      text: 'Logout',
+      cssClass: 'danger',
+      handler: () => {
+        this.navCtrl.navigateForward('/logout');
+      }
+    }
+    this.component.alertMessage('Warning', 'Are you sure you want to log out?', 'Cancel', btn);
   }
 
   toggleEdit() {
@@ -145,41 +122,7 @@ export class MorePage implements OnInit {
   /** Open page, manually check for third party pages. */
   async openPage(url: string, attachTicket = false) {
     if (!this.editMode) {
-      // external pages does not use relative or absolute link
-      if (url.startsWith('http://') || url.startsWith('https://')) {
-        // Manually exclude sites that do not need services ticket
-        if (!attachTicket) {
-          await this.openInAppBrowser(url);
-        } else {
-          const networkStatus = await Network.getStatus();
-          if (networkStatus.connected) {
-            this.cas.getST(url).subscribe(st => {
-              this.openInAppBrowser(`${url}?ticket=${st}`);
-            });
-          } else {
-            this.presentToast('External links cannot be opened in offline mode. Please ensure you have a network connection and try again');
-          }
-        }
-      } else {
-        url !== 'logout' ? this.navCtrl.navigateForward([url]) : this.logout();
-      }
+      await this.openInAppBrowser(url);
     }
-
-  }
-
-  async presentToast(msg: string) {
-    const toast = await this.toastCtrl.create({
-      message: msg,
-      color: 'danger',
-      duration: 6000,
-      position: 'top',
-      buttons: [
-        {
-          text: 'Close',
-          role: 'cancel'
-        }
-      ],
-    });
-    toast.present();
   }
 }
