60e226d2f979198e85271712c8d3d398d53c19c8
Muhammad Syafiq Nordin <muhammad.syafiq@apu.edu.my>
2022-11-08 15:12:17 +0800

    feat(migration): migrated dmu-form in profile page

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DmuFormPage } from './dmu-form/dmu-form.page';
 
 import { ProfilePage } from './profile.page';
 
@@ -11,7 +12,12 @@ const routes: Routes = [
   {
     path: 'visa-status',
     loadChildren: () => import('./visa-status/visa-status.module').then( m => m.VisaStatusPageModule)
+  },
+  {
+    path: 'dmu-form',
+    component: DmuFormPage
   }
+
 ];
 
 @NgModule({
