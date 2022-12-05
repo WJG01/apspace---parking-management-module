afe5a62fa9c6dee7feb4c7a2c44b738f72b8389d
Muhammad Faris <muhammad.farisfaisal03@gmail.com>
2022-12-05 12:31:38 +0800

    fix: incorrect message with svg

--- src/app/pages/library/library.module.ts
+++ src/app/pages/library/library.module.ts
@@ -6,13 +6,15 @@ import { IonicModule } from '@ionic/angular';
 import { LibraryPageRoutingModule } from './library-routing.module';
 import { LibraryPage } from './library.page';
 import { DaysLeftPipe } from './days-left/days-left.pipe';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LibraryPageRoutingModule
+    LibraryPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [LibraryPage, DaysLeftPipe]
 })
