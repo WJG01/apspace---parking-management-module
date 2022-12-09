eb2d8459f530b029fd3407f39cfa3bc43d34e181
Muhammad Faris <muhammad.farisfaisal03@gmail.com>
2022-12-09 14:10:06 +0800

    fix: teams button not working

--- src/app/services/app-launcher.service.ts
+++ src/app/services/app-launcher.service.ts
@@ -1,82 +1,40 @@
 import { Injectable } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { AppAvailability } from '@awesome-cordova-plugins/app-availability/ngx';
+import { AppLauncher } from '@capacitor/app-launcher';
+import { Capacitor } from '@capacitor/core';
 
 import { ComponentService } from './component.service';
-
-declare var detectBrowser;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppLauncherService {
 
-  constructor(
-    private plt: Platform,
-    private appAvailability: AppAvailability,
-    private component: ComponentService
-  ) { }
+  constructor(private component: ComponentService) { }
 
-  getPlatform(): string {
-    let platform: string;
-    if (this.plt.platforms().find(ele => ele === 'core')) {
-      platform = detectBrowser();
-    } else if (this.plt.platforms().find(ele => ele === 'android')) {
-      platform = 'Android';
-    } else if (this.plt.platforms().find(ele => ele === 'ios')) {
-      platform = 'iOS';
-    } else if (this.plt.platforms().find(ele => ele === 'windows')) {
-      platform = 'Window Mobile';
-    } else {
-      platform = this.plt.platforms().toString();
-    }
-    return platform;
-  }
-
-  launchExternalApp(
-    iosSchemaName: string,
-    androidPackageName: string,
-    webUrl: string,
-    playStoreUrl: string,
-    appStoreUrl: string,
-  ) {
-    // window.location.href = 'msteams://'
-    let app: string;
-    if (this.getPlatform() === 'iOS') {
-      app = iosSchemaName;
-    } else if (this.getPlatform() === 'Android') {
-      app = androidPackageName;
-    } else { // WEB
-      this.component.openLink(webUrl);
-      return;
-    }
-    this.appAvailability.check(app).then(
-      () => { // APP INSTALLED
-        window.location.href = `${webUrl}`;
-      },
-      () => { // APP IS NOT INSTALLED
-        if (this.getPlatform() === 'Android') {
-          this.component.openLink(playStoreUrl);
-        } else if (this.getPlatform() === 'iOS') {
-          this.component.openLink(appStoreUrl);
-        }
-      }
-    );
-  }
-
-  chatInTeams(userEmail: string) {
+  async chatInTeams(userEmail: string) {
+    // Native Scheme
     const androidSchemeUrl = 'com.microsoft.teams';
     const iosSchemeUrl = 'msteams://';
-    const webUrl= `https://teams.microsoft.com/l/chat/0/0?users=${userEmail}`;
+    // Web URL
+    const webUrl = `https://teams.microsoft.com/l/chat/0/0?users=${userEmail}`;
+    // Native Platform Url
     const appStoreUrl = 'https://itunes.apple.com/us/app/microsoft-teams/id1113153706?mt=8';
-    const playStoreUrl = `https://play.google.com/store/apps/details?id=com.microsoft.teams`;
+    const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.microsoft.teams';
+    // Get Platform
+    const platform = Capacitor.getPlatform().toLowerCase();
+    // Download url when Teams is not installed
+    const downloadUrl = platform === 'android' ? playStoreUrl : appStoreUrl;
+    // Scheme Used to open Teams
+    const openUrl = platform === 'android' ? androidSchemeUrl : iosSchemeUrl;
 
-    this.launchExternalApp(
-      iosSchemeUrl,
-      androidSchemeUrl,
-      webUrl,
-      appStoreUrl,
-      playStoreUrl,
-    );
+    if (platform === 'web') {
+      return this.component.openLink(webUrl);
+    }
+
+    const appLauncher = await AppLauncher.openUrl({ url: openUrl });
+
+    if (appLauncher.completed) return;
+
+    this.component.openLink(downloadUrl);
   }
 }
