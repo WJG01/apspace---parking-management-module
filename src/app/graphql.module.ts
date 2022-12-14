d18d33cc03e3758c6329f0cd6c8569e7b0ec2962
jiraichi <hi@shanvaliyev.com>
2022-12-14 17:01:42 +0800

    fixL attendix fix

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
-import { ApolloLink } from '@apollo/client/core';
-import { InMemoryCache } from '@apollo/client/cache';
-import { setContext } from '@apollo/client/link/context';
-import { HttpLink } from 'apollo-angular/http';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { AUTH_TYPE } from 'aws-appsync';
 import { createAuthLink } from 'aws-appsync-auth-link';
 import { createSubscriptionHandshakeLink } from 'aws-appsync-subscription-link';
 
 import { CasTicketService } from './services';
 
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
 export function createApollo(httpLink: HttpLink, cas: CasTicketService) {
   const url = 'https://attendix.apu.edu.my/graphql';
   const region = 'ap-southeast-1';
 
   const rfl = ApolloLink.from([
+    // eslint-disable-next-line arrow-body-style
     setContext(async (_request, previousContext: Record<string, any>) => {
       return ({
         headers: {
@@ -41,7 +43,7 @@ export function createApollo(httpLink: HttpLink, cas: CasTicketService) {
 }
 
 @NgModule({
-  exports: [ApolloModule],
+  exports: [ApolloModule, HttpLinkModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
