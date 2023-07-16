3a972136c67f072ca690af46cd74358e69f92a6a
WJ <anonymousname1904@gmail.com>
2023-07-16 16:45:48 +0800

    parking map positioning

--- src/app/pages/parking-map/parking-map.page.ts
+++ src/app/pages/parking-map/parking-map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { parkingPositions } from './parkingspot-pinpoint-position';
 
 @Component({
   selector: 'app-parking-map',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ParkingMapPage implements OnInit {
 
   chosenParkingZone = '';
+  chosenParkingSpot = '';
 
   locations = [
     { key: 'APU-A', value: 'Zone A - APU' },
@@ -21,12 +23,77 @@ export class ParkingMapPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.pinpointTop = 35;
-    this.pinpointLeft = 638;
+    // this.pinpointTop = 35;
+    //this.pinpointTop = 68;
+    //this.pinpointLeft = 638;
+
+    //for mobile left
+    // this.pinpointTop = 50;
+    // this.pinpointLeft = 240;
   }
 
   changeMap() {
 
+  }
+
+  // changeParkingSpot() {
+  //   const viewType = 'Desktop';
+  //   const parkinglocationType = this.chosenParkingZone;
+  //   const parkingspot = this.chosenParkingSpot;
+
+  //   const filteredSpots = parkingPositions
+  //     .filter(position => position.viewType === viewType)
+  //     .map(position => position.parkinglocationtype)
+  //     .flat() //console.log here
+  //     .filter(location => location.parkinglocation === parkinglocationType)
+  //     .map(location => location.spots)
+  //     .flat() //console.log here
+  //     .filter(spot => spot.parkingspotid === parkingspot);
+
+  //   if (filteredSpots.length > 0) {
+  //     const spot = filteredSpots[0]; // Assuming we only need the first spot
+
+  //     this.pinpointTop = spot.top;
+  //     this.pinpointLeft = spot.left;
+  //   } else {
+  //     // Handle case when no matching spots found
+  //   }
+
+  // }
+
+  changeParkingSpot() {
+    const viewType = 'Mobile';
+    const parkinglocationType = this.locations.find((location) => location.value === this.chosenParkingZone).key;
+    const parkingspot = this.chosenParkingSpot;
+
+    console.log('what spot is chosen', parkingspot);
+    const filteredSpots = parkingPositions
+      .filter(position => position.viewType === viewType)
+      .map(position => position.parkinglocationtype)
+      .flat();
+
+    console.log('Filtered spots after first .flat():', filteredSpots);
+
+    const filteredLocations = filteredSpots
+      .filter(location => location.parkinglocation === parkinglocationType)
+      .map(location => location.spots)
+      .flat();
+
+    console.log('Filtered locations after second .flat():', filteredLocations);
+
+    const filteredSpotsByID = filteredLocations
+      .filter(spot => spot.parkingspotid === parkingspot);
+
+    console.log('Filtered spots by ID:', filteredSpotsByID);
+
+    if (filteredSpotsByID.length > 0) {
+      const spot = filteredSpotsByID[0]; // Assuming we only need the first spot
+
+      this.pinpointTop = spot.top;
+      this.pinpointLeft = spot.left;
+    } else {
+      // Handle case when no matching spots found
+    }
   }
 
   getImageByLocation(location: string): string {
@@ -35,6 +102,10 @@ export class ParkingMapPage implements OnInit {
 
     // Construct the image source based on the value of the selected location
     return selectedLocation ? `assets/img/${selectedLocation.value}.jpg` : '';
+  }
+
+  generateNumberOptions(): string[] {
+    return Array.from({ length: 20 }, (_, index) => (index + 1).toString().padStart(2, '0'));
   }
 
 }
--- src/app/pages/parking-map/parkingspot-pinpoint-position.ts
+++ src/app/pages/parking-map/parkingspot-pinpoint-position.ts
@@ -1,7 +1,77 @@
 /* eslint-disable eol-last */
 export const parkingPositions = [
-    { parkingspotid: 'A1', top: 100, left: 200 },
-    { parkingspotid: 'A2', top: 150, left: 300 },
-    { parkingspotid: 'B1', top: 200, left: 400 },
-    // Add more parking spot positions as needed
+    {
+        viewType: 'Desktop', parkinglocationtype: [
+            {
+                parkinglocation: 'APU-A',
+                spots: Array.from(Array(20), (_, index) => {
+                    if (index === 19) {
+                        return {
+                            parkingspotid: '20',
+                            top: 35,
+                            left: 430
+                        };
+                    }
+                    return {
+                        parkingspotid: String(index + 1).padStart(2, '0'),
+                        top: 35 + (index * 33) + index * 5,
+                        left: 638
+                    };
+                })
+            },
+            {
+                parkinglocation: 'APU-B',
+                spots: [
+                    { parkingspotid: '01', top: 35, left: 638 },
+                    { parkingspotid: '02', top: 68, left: 638 },
+                    { parkingspotid: '03', top: 101, left: 638 }
+                ]
+            },
+            {
+                parkinglocation: 'APIIT-G',
+                spots: [
+                    { parkingspotid: '01', top: 35, left: 638 },
+                    { parkingspotid: '02', top: 68, left: 638 },
+                    { parkingspotid: '03', top: 101, left: 638 }
+                ]
+            }
+        ]
+    },
+    {
+        viewType: 'Mobile', parkinglocationtype: [
+            {
+                parkinglocation: 'APU-A',
+                spots: Array.from(Array(20), (_, index) => {
+                    if (index === 19) {
+                        return {
+                            parkingspotid: '20',
+                            top: 35,
+                            left: 150
+                        };
+                    }
+                    return {
+                        parkingspotid: String(index + 1).padStart(2, '0'),
+                        top: 35 + (index * 15) + index *1.5,
+                        left: 240
+                    };
+                })
+            },
+            {
+                parkinglocation: 'APU-B',
+                spots: [
+                    { parkingspotid: '01', top: 35, left: 638 },
+                    { parkingspotid: '02', top: 68, left: 638 },
+                    { parkingspotid: '03', top: 101, left: 638 }
+                ]
+            },
+            {
+                parkinglocation: 'APIIT-G',
+                spots: [
+                    { parkingspotid: '01', top: 35, left: 638 },
+                    { parkingspotid: '02', top: 68, left: 638 },
+                    { parkingspotid: '03', top: 101, left: 638 }
+                ]
+            }
+        ]
+    }
 ];
